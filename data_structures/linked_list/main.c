db3de852e5e58903aa02a0d46d75d2630e4f7dc5
chendu471 <chandrashekarpabbaraju@gmail.com>
2021-01-16 10:05:45 +0530

    added

--- data_structures/linked_list/main.c
+++ data_structures/linked_list/main.c
@@ -3,18 +3,78 @@
 
 int main(void)
 {
-int count=0,data=0;
-struct node *head=NULL;
-printf("Enter no of nodes to create : ");
-scanf("%d",&count);
-while(count)
-{
-printf("Enter data : ");
-scanf("%d",&data);
-head=add_node_at_end(head,data);
-count--;
+	int count=0,data=0,option=0,pos=0;
+	struct node *head=NULL;
+	//	printf("Enter no of nodes to create : ");
+	//	scanf("%d",&count);
+	while(option !=5)
+	{
+		printf("Enter your choice \t 	1.add at end \n \
+				2.add at begin \n \
+				3.add at position \n\
+				4.delete node\n \
+				5.stop app\n");
+
+		scanf("%d",&option);
+		if(option<4)
+		{
+
+			printf("Enter data : ");
+			scanf("%d",&data);
+		}
+		switch(option)
+		{
+			case 1:
+				head=add_node_at_end(head,data);
+				break;
+			case 2:
+
+				head=add_at_pos(head,data,1);	
+				break;	
+			case 3:
+				printf("enter position: ");
+				scanf("%d",&pos);
+				count=count_list(head);
+				if(pos>count+1)
+				{
+					printf("Invalid position\n");
+				}
+				else
+				{
+					head=add_at_pos(head,data,pos);
+					break;
+				}
+			case 4:
+				printf("enter position: ");
+				scanf("%d",&pos);
+				count=count_list(head); 
+				if(pos>count)
+				{
+					printf("Invalid position\n");
+
+				}
+				else
+				{
+					head=del_at_pos(head,pos);		
+				}
+	
+		}
+
+		print_list(head);
+		count--;
+	}
+
+	return 0;
 }
 
-print_list(head);
-return 0;
+
+int count_list(struct node* head)
+{
+	int count=0;
+	while(head)
+	{
+		head=head->next;
+		count++;
+	}
+	return count;
 }
--- data_structures/linked_list/myheaders.h
+++ data_structures/linked_list/myheaders.h
@@ -1,6 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
-
+#include<string.h>
 
 struct node{
 int data;
@@ -9,3 +9,8 @@ struct node *next;
 
 struct node* add_node_at_end(struct node *head,int data);
 void print_list(struct node *head);
+struct node* add_at_pos(struct node *head,int data,int pos);
+struct node *del_at_pos(struct node *head,int pos);
+int count_list(struct node *head);
+
+int count_list(struct node *head);
